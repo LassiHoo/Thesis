bb5d87c15ceb2e64e32a5f3ed745a62cf1fbffa7
LassiPee <lassi@hirvonen.net>
2017-11-19 17:26:24 +0200

    End node and tx commands added

--- FileHandler/FileHandler.py
+++ FileHandler/FileHandler.py
@@ -4,9 +4,18 @@ import pickle
 class fileHandler:
 
     transmitterList = []
+    initList=[]
+    commandIndex=[]
     sendInterval = 1
+    sendCount = 10
     payLoadLenght = 10
     startTime = ""
+    isDutyCycleOn = false
+
+    def dutyCycleOn():
+        #create dutyCycle calcutalion here
+        return False
+
 
     def fileHandleOpen(name, RW, errorString, dump):
         #opens a transmitter or initialization file
@@ -19,8 +28,10 @@ class fileHandler:
             pickle.dump(dump, source)
             return source
 
-class LoraWANTransmitterListHandler(fileHandler):
-    #these are LoraWan end node related configuration commands
+class loraWANTxListHandler(fileHandler):
+    #these are LoraWan end node related configuration  and TX send commands
+    #
+
     MacSet = "mac set "
     MacTx = "mac tx "
     Conf = "cnf "
@@ -30,8 +41,26 @@ class LoraWANTransmitterListHandler(fileHandler):
     Appkey = "appkey 660625ED5FC16D37B82A5A0E9042CF0B"
     DevEui = "deveui 0004A30B001F3A95"
     macSave = "mac save"
+    RLF = "\r\n"
 
+    def commandIndex(self):
 
+        fileHandler.commandIndex = "START_TIME" "SEND_INTERVAL" "SEND_COUNT" "PAYLOAD_LENGHT" "TX_COMMAND""LINE_FEED"
 
+    def  createLoraWanTransmitFile(self):
+        #creating list
+        fileHandler.transmitterList.append(fileHandler.startTime)
+        fileHandler.transmitterList.append(fileHandler.sendInterval)
+        fileHandler.transmitterList.append(fileHandler.sendCount)
+        fileHandler.transmitterList.append(fileHandler.payLoadLenght)
+        fileHandler.transmitterList.append(self.MacTx + self.portnr )
+        fileHandler.transmitterList.append(self.RLF)
+
+    def createInitList(self):
+        #creating init list
+        fileHandler.initList.append(self.MacSet + self.Appeui + self.RLF )
+        fileHandler.initList.append(self.MacSet + self.Appkey + self.RLF)
+        fileHandler.initList.append(self.MacSet + self.DevEui + self.RLF)
+        fileHandler.initList.append(self.macSave + self.RLF)
 
 #class NBiotTransmitterList
