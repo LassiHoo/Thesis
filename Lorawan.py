8d2649378709300d75c22ff5c76c05fdbe1c17b9
Lassi Hirvonen <lassi@hirvonen.net>
2018-01-03 22:01:26 +0200

    ...

--- Lorawan.py
+++ Lorawan.py
@@ -3,48 +3,6 @@ from BasePlatform import basePlatform
 from tcp_ip import ssh_connection
 import time
 
-
-class LoraWan:
-    # this must be asked first in the start up
-    Port = '/dev/ttyACM0'
-    __platForm = loraWanPlatfom()
-    __init = basePlatform()
-    __initcommands=[]
-    __transmitcommands=[]
-
-    def __init__(self, ):
-        self.__loraWanCom = serial.Serial(port=self.Port,
-                                   baudrate=57600, parity=serial.PARITY_NONE,
-                                   stopbits=serial.STOPBITS_ONE,
-                                   bytesize=serial.EIGHTBITS,
-                                   timeout=1)
-
-        # create transmit and initializaiotn files if they do not exists
-        self.__platForm.createTransmitFile()
-        self.__platForm.createInitializationFile()
-
-        self.__transmitcommands = self.__init.readfile(self.__platForm.transmitFileName)
-        for i in self.__transmitcommands:
-            print i
-        self.__initcommands = self.__init.readfile(self.__platForm.initFileName)
-        for i in self.__initcommands:
-            print i
-
-    def transmit(self,string):
-        self.__loraWanCom.write(self.__transmitcommands[loraWanPlatfom.TX_COMMAND] + string + self.__transmitcommands[loraWanPlatfom.LINE_FEED])
-
-    def initInterface(self):
-        for i in self.__initcommands:
-            time.sleep(1)
-            print(i)
-            self.__loraWanCom.write(i)
-
-    def start_gateway_logging(self):
-        # log into gateway and start to tcp dump
-        ssh = ssh_connection()
-        ssh.Login('192.168.0.119', 'pi', 'raspberry')
-        ssh.StartTCPdump("testfile.dat")
-
 class loraWanPlatfom(basePlatform):
     #these are LoraWan end node related configuration  and TX instruction set
 
@@ -96,3 +54,44 @@ class loraWanPlatfom(basePlatform):
             source = open(self.transmitFileName,"wb")
             pickle.dump(basePlatform.transmitterList, source)
             source.close()
+            
+class LoraWan:
+    # this must be asked first in the start up
+    Port = '/dev/ttyACM0'
+    __platForm = loraWanPlatfom()
+    __init = basePlatform()
+    __initcommands=[]
+    __transmitcommands=[]
+
+    def __init__(self, ):
+        self.__loraWanCom = serial.Serial(port=self.Port,
+                                   baudrate=57600, parity=serial.PARITY_NONE,
+                                   stopbits=serial.STOPBITS_ONE,
+                                   bytesize=serial.EIGHTBITS,
+                                   timeout=1)
+
+        # create transmit and initializaiotn files if they do not exists
+        self.__platForm.createTransmitFile()
+        self.__platForm.createInitializationFile()
+
+        self.__transmitcommands = self.__init.readfile(self.__platForm.transmitFileName)
+        for i in self.__transmitcommands:
+            print i
+        self.__initcommands = self.__init.readfile(self.__platForm.initFileName)
+        for i in self.__initcommands:
+            print i
+
+    def transmit(self,string):
+        self.__loraWanCom.write(self.__transmitcommands[loraWanPlatfom.TX_COMMAND] + string + self.__transmitcommands[loraWanPlatfom.LINE_FEED])
+
+    def initInterface(self):
+        for i in self.__initcommands:
+            time.sleep(1)
+            print(i)
+            self.__loraWanCom.write(i)
+
+    def start_gateway_logging(self):
+        # log into gateway and start to tcp dump
+        ssh = ssh_connection()
+        ssh.Login('192.168.0.119', 'pi', 'raspberry')
+        ssh.StartTCPdump("testfile.dat")
