606c37de00a573021f841af1d29d6beedc30c45e
Lassi Hirvonen <lassi@hirvonen.net>
2018-01-16 23:06:32 +0200

    .

--- Transceiver.py
+++ Transceiver.py
@@ -38,7 +38,7 @@ def main():
          date = datetime.datetime.utcnow()
          total_milliseconds = ( date.microsecond / 1000) + ( date.minute * 60 * 1000 ) + (date.second * 1000)
          add = hex(random.randint(0,20))[2:]
-         wapice_test_line_hex += add
+         wapice_test_line_hex = add
          transmit_log_file.addTxData(wapice_test_line_hex,total_milliseconds)
          trdelay.append(total_milliseconds)
          lpwa_interface.transmit(wapice_test_line_hex)
