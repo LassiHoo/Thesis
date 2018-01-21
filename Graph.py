bd8bf4b304617e2b9e666ba66e4cb287199753ab
Lassi Hirvonen <lassi@hirvonen.net>
2018-01-22 00:21:47 +0200

    -

--- Graph.py
+++ Graph.py
@@ -1,5 +1,6 @@
 import plotly
 from plotly import tools
+import plotly.graph_objs as go
 from plotly.graph_objs import *
 import plotly.plotly as py
 import numpy as np
@@ -37,42 +38,47 @@ class graph:
         # py.plot(data, filename='basic-line')
         cumsum = np.cumsum(y2)
 
-        trace6 = Scatter(x=[i for i in range(len(cumsum))], y=10 * cumsum / np.linalg.norm(cumsum),
+        trace = Scatter(x=[i for i in range(len(cumsum))], y=10 * cumsum / np.linalg.norm(cumsum),
                            marker=dict(color='rgb(150, 25, 120)'))
+        layout = go.Layout(
+            title="Cumulative Distribution Function"
+        )
 
+        fig = go.Figure(data=go.Data([trace]), layout=layout)
+        py.plot(fig, filename='cdf-dataset')
 
-        trace1 = Scatter(
-            x= x,
-            y= y1,
-            name=y1_name,
-        )
-        trace2 = Scatter(
-            x=x,
-            y=y2,
-            name = y2_name,
-        )
-        trace3 = Scatter(
-            x=x,
-            y=y3,
-            name = y3_name,
-        )
-        trace4 = Scatter(
-            x=x,
-            y=y4,
-            name = y4_name,
-        )
-        trace5 = Scatter(
-            x=x,
-            y=y5,
-            name=y5_name,
-        )
-        fig = tools.make_subplots(rows=5, cols=1)
-
-        fig.append_trace(trace3, 1, 1)
-        fig.append_trace(trace2, 2, 1)
-        fig.append_trace(trace1, 3, 1)
-        fig.append_trace(trace4, 4, 1)
-        fig.append_trace(trace5, 5, 1)
-        fig.append_trace(trace6, 6, 1)
-        fig['layout'].update(height=600, width=600, title='Stacked subplots')
-        py.plot(fig, filename='stacked-subplots')
+        # trace1 = Scatter(
+        #     x= x,
+        #     y= y1,
+        #     name=y1_name,
+        # )
+        # trace2 = Scatter(
+        #     x=x,
+        #     y=y2,
+        #     name = y2_name,
+        # )
+        # trace3 = Scatter(
+        #     x=x,
+        #     y=y3,
+        #     name = y3_name,
+        # )
+        # trace4 = Scatter(
+        #     x=x,
+        #     y=y4,
+        #     name = y4_name,
+        # )
+        # trace5 = Scatter(
+        #     x=x,
+        #     y=y5,
+        #     name=y5_name,
+        # )
+        # fig = tools.make_subplots(rows=5, cols=1)
+        #
+        # fig.append_trace(trace3, 1, 1)
+        # fig.append_trace(trace2, 2, 1)
+        # fig.append_trace(trace1, 3, 1)
+        # fig.append_trace(trace4, 4, 1)
+        # fig.append_trace(trace5, 5, 1)
+        # fig.append_trace(trace6, 6, 1)
+        # fig['layout'].update(height=600, width=600, title='Stacked subplots')
+        # py.plot(fig, filename='stacked-subplots')
