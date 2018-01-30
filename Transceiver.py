5bd0fac179573280db09de5d89e5fe423df2a2de
Lassi Hirvonen <lassi@hirvonen.net>
2018-01-30 19:16:18 +0200

    .

--- Transceiver.py
+++ Transceiver.py
@@ -41,7 +41,7 @@ def main():
          time.sleep(sleep_time)
          interval_delay.append(sleep_time)
          transmissioncount.append(i)
-         date = datetime.datetime.now()
+         date = datetime.datetime.utcnow()
          total_milliseconds = ( date.microsecond / 1000) + ( date.minute * 60 * 1000 ) + (date.second * 1000)
          datemilliseocndhex = hex( total_milliseconds)[2:]
          print("date in hex ", datemilliseocndhex)
