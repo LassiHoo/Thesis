19debad9d3cc8a1014e3b9d08ab71af896c647c3
Lassi Hirvonen <lassi@hirvonen.net>
2018-02-01 19:21:26 +0200

    .

--- BasePlatform.py
+++ BasePlatform.py
@@ -8,7 +8,7 @@ class basePlatform:
     initList=[]
     commandIndex=[]
     # milliseconds
-    sendInterval = 5000
+    sendInterval = 8000
     sendCount = 10
     payLoadLenght = 10
     startTime = ""
