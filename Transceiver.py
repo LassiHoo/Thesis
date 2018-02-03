6947a26a7842cec52c71389286e251241a3b1d56
Lassi Hirvonen <lassi@hirvonen.net>
2018-02-03 23:01:05 +0200

    ,

--- Transceiver.py
+++ Transceiver.py
@@ -10,32 +10,35 @@ import threading
 from Graph import graph
 pi = 0
 def transmit_thread_function(transmit_settings,lpwa_interface):
+
+
+    print("thread started transmitter settings ", transmit_settings)
     pi = 0
-    while True:
-
-         dec = (int(pi)*transmit_settings.return_transmit_settings('interval_decrement_milliseconds'))
-         if dec > transmit_settings.return_transmit_settings('send_interval_milliseconds'):
-             pi = 0
-             dec = 0
-         sleep_time = transmit_settings.return_transmit_settings('send_interval_milliseconds' - dec)/1000.0
-         #sleep_time = (transmit_settings.sendInterval/1000.0)
-         print("sleep time: ",sleep_time)
-         time.sleep(sleep_time)
-         date = datetime.datetime.utcnow()
-         total_milliseconds = ( date.microsecond / 1000) + ( date.minute * 60 * 1000 ) + (date.second * 1000)
-         print("date in dec ", total_milliseconds)
-         #datemilliseocndhex = hex( total_milliseconds)[2:]
-         frame = str(total_milliseconds) + ":" + str(pi) + ":" + str(sleep_time)
-         #print("date in hex ", datemilliseocndhex)
-         frame_hex = binascii.hexlify(frame)
-         add = hex(random.randint(0,20))[2:]
-         wapice_test_line_hex = add
-         #transmit_log_file.addTxData(wapice_test_line_hex,total_milliseconds)
-         #trdelay.append(total_milliseconds)
-         if transmit_settings.return_transmit_settings('data_content') == 'from_diagnostic_frame':
-             lpwa_interface.transmit(frame_hex)
-
-         pi = pi + 1
+    # while True:
+    # 
+    #      dec = (int(pi)*transmit_settings.return_transmit_settings('interval_decrement_milliseconds'))
+    #      if dec > transmit_settings.return_transmit_settings('send_interval_milliseconds'):
+    #          pi = 0
+    #          dec = 0
+    #      sleep_time = transmit_settings.return_transmit_settings('send_interval_milliseconds' - dec)/1000.0
+    #      #sleep_time = (transmit_settings.sendInterval/1000.0)
+    #      print("sleep time: ",sleep_time)
+    #      time.sleep(sleep_time)
+    #      date = datetime.datetime.utcnow()
+    #      total_milliseconds = ( date.microsecond / 1000) + ( date.minute * 60 * 1000 ) + (date.second * 1000)
+    #      print("date in dec ", total_milliseconds)
+    #      #datemilliseocndhex = hex( total_milliseconds)[2:]
+    #      frame = str(total_milliseconds) + ":" + str(pi) + ":" + str(sleep_time)
+    #      #print("date in hex ", datemilliseocndhex)
+    #      frame_hex = binascii.hexlify(frame)
+    #      add = hex(random.randint(0,20))[2:]
+    #      wapice_test_line_hex = add
+    #      #transmit_log_file.addTxData(wapice_test_line_hex,total_milliseconds)
+    #      #trdelay.append(total_milliseconds)
+    #      if transmit_settings.return_transmit_settings('data_content') == 'from_diagnostic_frame':
+    #          lpwa_interface.transmit(frame_hex)
+    #
+    #      pi = pi + 1
 
 
 def main():
