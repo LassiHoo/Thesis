4567e66d9c20c6bea62aace5dc882fd2101c92cf
Lassi Hirvonen <lassi@hirvonen.net>
2018-02-03 23:56:32 +0200

    .

--- Transceiver.py
+++ Transceiver.py
@@ -13,8 +13,9 @@ def transmit_thread_function(transmit_settings,lpwa_interface):
 
 
     print("thread started transmitter settings ", transmit_settings)
-    if (transmit_settings[0]['start_time'] == 'dummy'):
-        print("dummy detected")
+    print(transmit_settings)
+    #if (transmit_settings[0]['start_time'] == 'dummy'):
+    #    print("dummy detected")
     pi = 0
     # while True:
     #
